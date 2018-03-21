3cbca139e3c11f4ac456715d7c7412d7d688d48f
SAAS <none@none>
2018-03-21 22:27:18 +0300

    Перенёс менюшки с правой панельки.
    --HG--
    branch : sdk

--- editors/LevelEditor/main.cpp
+++ editors/LevelEditor/main.cpp
@@ -32,6 +32,10 @@ TfrmMain *frmMain;
 #include "../../xrSound/soundrender_source.h"
 #include "ResourceManager.h"
 #include "../xrEProps/EditorChooseEvents.h"
+#include "UI_LevelTools.h"
+#include "UI_LevelMain.h"
+#include "FrmDBXpacker.h"
+#include "scene.h"
 
 
 __fastcall TfrmMain::TfrmMain(TComponent* Owner)
@@ -54,6 +58,54 @@ __fastcall TfrmMain::TfrmMain(TComponent* Owner)
     if (!ExecCommand(COMMAND_INITIALIZE,(u32)D3DWindow,(u32)paRender)){ 
     	FlushLog			();
     	TerminateProcess(GetCurrentProcess(),-1);
+    }
+
+    // add tools to edit mode menu
+    struct
+    {
+    	LPCSTR caption;
+        DWORD class_id;
+    } tools_desc[] = {
+    	{"Object",			OBJCLASS_SCENEOBJECT},
+        {"Light",			OBJCLASS_LIGHT},
+        {"Sound Source",	OBJCLASS_SOUND_SRC},
+        {"Sound Env",		OBJCLASS_SOUND_ENV},
+        {"Glow",			OBJCLASS_GLOW},
+        {"Shape",			OBJCLASS_SHAPE},
+        {"Spawn Element",	OBJCLASS_SPAWNPOINT},
+        {"Way Points",		OBJCLASS_WAY},
+        {"Sector",			OBJCLASS_SECTOR},
+        {"Portal",			OBJCLASS_PORTAL},
+        {"Group",			OBJCLASS_GROUP},
+        {"Static Particles",OBJCLASS_PS},
+        {"Detail Objects",	OBJCLASS_DO},
+        {"AI-Map",			OBJCLASS_AIMAP},
+        {"Wallmarks",		OBJCLASS_WM},
+        {"Fog Volumes",		OBJCLASS_FOG_VOL}
+    };
+
+    for(int i = 0; i < sizeof(tools_desc)/sizeof(tools_desc[0]); i++)
+    {
+    	TMenuItem *item 	= xr_new<TMenuItem>(this);
+    	item->Caption 		= tools_desc[i].caption;
+    	item->Tag			= tools_desc[i].class_id;
+        item->RadioItem		= true;
+        item->OnClick		= miChangeTargetClick;
+
+        miEditMode->Add		(item);
+    }
+
+    for(i = 0; i < sizeof(tools_desc)/sizeof(tools_desc[0]); i++)
+    {
+    	TMenuItem *item		= xr_new<TMenuItem>(this);
+        item->Caption		= "";
+        item->Tag			= tools_desc[i].class_id;
+        item->OnClick		= miShowTargetClick;
+
+        if(i == 0)
+        	item->Break		= mbBreak;
+
+        miEditMode->Add		(item);
     }
 }
 //---------------------------------------------------------------------------
@@ -179,14 +231,47 @@ void __fastcall TfrmMain::UpdateCaption()
 }
 //---------------------------------------------------------------------------
 
+void TfrmMain::RefreshBars()
+{
+    fraTopBar->RefreshBar		();
+    fraLeftBar->RefreshBar		();
+    fraBottomBar->RefreshBar	();
+
+    miOpenRecent->Clear();
+    for(size_t i = 0; i < EPrefs->scene_recent_list.size(); i++)
+    {
+    	TMenuItem *item = xr_new<TMenuItem>(this);
+        item->Caption = EPrefs->scene_recent_list[i];
+        item->Tag = i;
+        item->OnClick = miOpenRecentClick;
+
+        miOpenRecent->Add(item);
+    }
+
+    for(i = 0; i < miEditMode->Count/2; i++)
+    {
+    	if(LTools->CurrentClassID() == miEditMode->Items[i]->Tag)
+        	miEditMode->Items[i]->Checked = true;
+    }
+
+    for(i = miEditMode->Count/2; i < miEditMode->Count; i++)
+    {
+    	ESceneToolBase *tool = Scene->GetTool(miEditMode->Items[i]->Tag);
+        VERIFY(tool);
+
+        miEditMode->Items[i]->ImageIndex = tool->IsVisible() ? 0 : 1;
+    }
+}
+//---------------------------------------------------------------------------
+
 void __fastcall TfrmMain::tmRefreshTimer(TObject *Sender)
 {
-    int i;
-    for (i=0; i<frmMain->ComponentCount; i++){
+    for (int i=0; i<frmMain->ComponentCount; i++){
         TComponent* temp = frmMain->Components[i];
         if (dynamic_cast<TExtBtn *>(temp) != NULL)
             ((TExtBtn*)temp)->UpdateMouseInControl();
     }
+
 	fraLeftBar->OnTimer();
 	fraTopBar->OnTimer();
 }
@@ -265,4 +350,386 @@ void __fastcall TfrmMain::fsStorageSavePlacement(TObject *Sender)
     fsStorage->WriteInteger("window_state",frmMain->WindowState);
 }
 //---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miClearClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_CLEAR);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miOpenClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_LOAD);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miSaveClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_SAVE, xr_string(LTools->m_LastFileName.c_str()));
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miSaveAsClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_SAVE, 0, 1);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miQuitClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_QUIT);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miOpenRecentClick(TObject *Sender)
+{
+	TMenuItem *item = (TMenuItem*)Sender;
+    ExecCommand(COMMAND_LOAD, xr_string(EPrefs->scene_recent_list[item->Tag].c_str()));
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miOpenSelectionClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_LOAD_SELECTION);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miSaveSelectionClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_SAVE_SELECTION);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miMakePackClick(TObject *Sender)
+{
+	DB_packer->ActivatePacker();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miUndoClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_UNDO);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miRedoClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_REDO);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miCutClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_CUT);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miCopyClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_COPY);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miPasteClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_PASTE);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miDeleteClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_DELETE_SELECTION);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miSelectAllClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_SELECT_ALL);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miDeselectAllClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_DESELECT_ALL);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miInvertSelectionClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_INVERT_SELECTION_ALL);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miChangeTargetClick(TObject *Sender)
+{
+	TMenuItem *item = (TMenuItem*)Sender;
+    ExecCommand(COMMAND_CHANGE_TARGET, item->Tag);
+    item->Checked = true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miShowTargetClick(TObject *Sender)
+{
+	TMenuItem *item = (TMenuItem*)Sender;
+    ESceneToolBase *tool = Scene->GetTool(item->Tag);
+    VERIFY(tool);
+
+    ExecCommand(COMMAND_SHOW_TARGET, item->Tag, !tool->IsVisible());
+    item->ImageIndex = tool->IsVisible() ? 0 : 1;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miBuildClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_BUILD);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miMakeGameClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_MAKE_GAME);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miMakeDetailsClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_MAKE_DETAILS);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miMakeHOMClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_MAKE_HOM);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miMakeSoundOccluderClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_MAKE_SOM);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::miMakeAIMapClick(TObject *Sender)
+{
+	ExecCommand(COMMAND_MAKE_AIMAP);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::Options1Click(TObject *Sender)
+{
+	ExecCommand(COMMAND_OPTIONS, CCommandVar("Scene"));	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::Validate1Click(TObject *Sender)
+{
+	ExecCommand(COMMAND_VALIDATE_SCENE);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::SummaryInfo1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_CLEAR_SCENE_SUMMARY );
+	ExecCommand( COMMAND_COLLECT_SCENE_SUMMARY );
+	ExecCommand( COMMAND_SHOW_SCENE_SUMMARY );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::HighlightTexture1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_SCENE_HIGHLIGHT_TEXTURE );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ClearDebugDraw1Click(TObject *Sender)
+{
+	ExecCommand(COMMAND_CLEAR_DEBUG_DRAW);	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ExportentireSceneasObj1Click(TObject *Sender)
+{
+	Scene->ExportObj(false);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::Exportselection1Click(TObject *Sender)
+{
+	Scene->ExportObj(true);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ImportErrorList1Click(TObject *Sender)
+{
+	ExecCommand(COMMAND_IMPORT_COMPILER_ERROR);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ExportErrorList1Click(TObject *Sender)
+{
+	ExecCommand(COMMAND_EXPORT_COMPILER_ERROR);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ClearErrorList1Click(TObject *Sender)
+{
+	ExecCommand(COMMAND_CLEAR_DEBUG_DRAW);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::LibraryEditor1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_LIBRARY_EDITOR );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ReloadObjects1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_RELOAD_OBJECTS );
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::CleanLibrary1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_CLEAN_LIBRARY );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ClipEditor1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_SHOW_CLIP_EDITOR) ;	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ImageEditor1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_IMAGE_EDITOR );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ReloadTextures1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_RELOAD_TEXTURES );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::SychronizeTextures1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_REFRESH_TEXTURES );
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::CheckNewTextures1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_CHECK_TEXTURES );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::EditMinimap1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_MINIMAP_EDITOR );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::SynchronizeTHM1Click(TObject *Sender)
+{
+	FS_FileSet      files;
+	FS.file_list    (files,_textures_,FS_ListFiles,"*.thm");
+	FS_FileSet::iterator I = files.begin();
+	FS_FileSet::iterator E = files.end();
+
+	for(; I!=E; ++I)
+	{
+		ETextureThumbnail* TH = xr_new<ETextureThumbnail>( (*I).name.c_str(), false);
+		TH->Load((*I).name.c_str(),_textures_);
+		TH->Save();
+		xr_delete(TH);
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::SoundEditor1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_SOUND_EDITOR );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::SynchronizeSounds1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_SYNC_SOUNDS );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::RefreshEnvironmentLibrary1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_REFRESH_SOUND_ENVS );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::RefreshEnvironmentGeometry1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_REFRESH_SOUND_ENV_GEOMETRY );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::LoadSoundOccluder1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_LOAD_SOUND_OCCLUDER );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::HideSelected1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_HIDE_SEL, FALSE );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::HideUnselected1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_HIDE_UNSEL );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::HideAll1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_HIDE_ALL, TRUE );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::Unhideall1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_HIDE_ALL, FALSE);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::MultiRename1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_MULTI_RENAME_OBJECTS );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::Properties1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_SHOW_PROPERTIES );
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::LightAnimEditor1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_LANIM_EDITOR );	
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::ObjectList1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_SHOW_OBJECTLIST );
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TfrmMain::Preferences1Click(TObject *Sender)
+{
+	ExecCommand( COMMAND_EDITOR_PREF );
+}
+//---------------------------------------------------------------------------
+
 
