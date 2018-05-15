0d6581aa97b34ebefd4b7624464599f625a24228
SAAS <none@none>
2018-05-15 23:23:52 +0300

    Issue #200 и Issue #202
    --HG--
    branch : sdk

--- editors/LevelEditor/FrameObject.cpp
+++ editors/LevelEditor/FrameObject.cpp
@@ -130,10 +130,10 @@ void TfraObject::SelByRefObject( bool flag )
 void __fastcall TfraObject::ebMultiAppendClick(TObject *Sender)
 {
 	LPCSTR N;
-    if (TfrmChoseItem::SelectItem(smObject,N,32,0)){
+    if (TfrmChoseItem::SelectItem(smObject,N,1024,0)){
     	Fvector pos={0.f,0.f,0.f};
     	Fvector up={0.f,1.f,0.f};
-        Scene->SelectObjects(false,OBJCLASS_SCENEOBJECT);
+//		Scene->SelectObjects(false,OBJCLASS_SCENEOBJECT); // Issue #200
 	    AStringVec lst;
     	_SequenceToList(lst,N);
         SPBItem* pb = UI->ProgressStart(lst.size(),"Append object: ");
