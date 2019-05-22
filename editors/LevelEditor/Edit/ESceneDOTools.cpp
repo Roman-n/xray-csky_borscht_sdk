28bb43980be33d5a8d762bbf52bf8b0b22f6f69f
saas <none@none>
2019-05-22 22:23:43 +0300

    Увеличил максимальный размер детальной текстуры до 16384x16384 Дальше реально нет смысла увеличивать
    --HG--
    branch : sdk

--- editors/LevelEditor/Edit/ESceneDOTools.cpp
+++ editors/LevelEditor/Edit/ESceneDOTools.cpp
@@ -489,7 +489,7 @@ bool EDetailManager::Export(LPCSTR path)
 	    	*remap_object_it	= (u8)new_idx++;
 
     AnsiString 			do_tex_name = ChangeFileExt(fn,"_details");
-    int res				= ImageLib.CreateMergedTexture(textures,do_tex_name.c_str(),STextureParams::tfDXT3,256,4096,256,4096,offsets,scales,rotated,remap);
+    int res				= ImageLib.CreateMergedTexture(textures,do_tex_name.c_str(),STextureParams::tfDXT3,256,16384,256,16384,offsets,scales,rotated,remap);
     if (1!=res)			bRes=FALSE;
 
     pb->Inc				("export geometry");
