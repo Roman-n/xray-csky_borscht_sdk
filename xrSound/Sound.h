2d94ae8cfebcea73a10d5e78150cd3713cb43936
saas <none@none>
2019-06-03 14:19:34 +0300

    Убрал двухстадиевую инициализацию звука сделанную в угоду защиты от копироваия (так же сделали в OpenXray ещё в 2016-м, кстати) + заменил CHECK_OR_EXIT на сообщения в лог, чтобы запускалось при отсутствиии звуковухи.
    --HG--
    branch : sdk

--- xrSound/Sound.h
+++ xrSound/Sound.h
@@ -237,7 +237,7 @@ typedef		void __stdcall sound_event						(ref_sound_data_ptr S, float range);
 /// definition (Sound Manager Interface)
 class XRSOUND_API	CSound_manager_interface
 {
-	virtual void	  				_initialize				(int stage)																			= 0;
+	virtual void	  				_initialize				( )																			= 0;
 	virtual void					_clear					( )																						= 0;
 
 protected:
@@ -247,7 +247,7 @@ protected:
 public:
 	virtual							~CSound_manager_interface(){}
 
-	static void						_create					(int stage);
+	static void						_create					( );
 	static void						_destroy				( );
 
 	virtual void					_restart				( )																						= 0;
