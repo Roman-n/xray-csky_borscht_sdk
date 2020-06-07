8ebcf3e4ee9fa8f4df4784a3d1970a6e3af93121
abramcumner <abramcumner@yandex.ru>
2020-06-07 19:53:17 +0300

    настроена сборка x86-release под msvc.

--- xray/editors/LevelEditor/ImGui/IM_FrameObject.cpp
+++ xray/editors/LevelEditor/ImGui/IM_FrameObject.cpp
@@ -38,15 +38,17 @@ LPCSTR _CopyVal ( LPCSTR src, AnsiString& dst, char separator )
 	return		dst.c_str();
 }
 
-//LPCSTR _GetItem ( LPCSTR src, int index, AnsiString& dst, char separator=',', LPCSTR def="", bool trim=true )
-//{
-//	LPCSTR	ptr;
-//	ptr			= _SetPos	( src, index, separator );
-//	if( ptr )	_CopyVal	( ptr, dst, separator );
-//	else	dst = def;
-//	if (trim)	dst			= Trim(dst);
-//	return		dst.c_str();
-//}
+#ifndef __MINGW32__
+LPCSTR _GetItem ( LPCSTR src, int index, AnsiString& dst, char separator=',', LPCSTR def="", bool trim=true )
+{
+	LPCSTR	ptr;
+	ptr			= _SetPos	( src, index, separator );
+	if( ptr )	_CopyVal	( ptr, dst, separator );
+	else	dst = def;
+	if (trim)	dst			= Trim(dst);
+	return		dst.c_str();
+}
+#endif
 
 void _SequenceToList(AStringVec& lst, LPCSTR in, char separator=',')
 {
