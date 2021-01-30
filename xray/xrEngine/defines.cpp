bac15f3a07e322f82b845590ca610c9a0f287728
abramcumner <abramcumner@yandex.ru>
2021-01-30 20:23:57 +0300

    debug: изменен параметры запуска по умолчанию отладочной сборки на оконный режим.

--- xray/xrEngine/defines.cpp
+++ xray/xrEngine/defines.cpp
@@ -9,7 +9,11 @@
 u32			psCurrentVidMode[2] = {1024,768};
 u32			psCurrentBPP		= 32;
 // release version always has "mt_*" enabled
+#ifdef DEBUG
+Flags32		psDeviceFlags		= {             rsDetails|mtPhysics|mtSound|mtNetwork|rsDrawStatic|rsDrawDynamic};
+#else
 Flags32		psDeviceFlags		= {rsFullscreen|rsDetails|mtPhysics|mtSound|mtNetwork|rsDrawStatic|rsDrawDynamic};
+#endif
 
 // textures 
 int			psTextureLOD		= 0;
